7f18fe40eea553acb52ce84eb7c6ebad449cddc4
Adrian Plesner <adrian.plesner@gmail.com>
2019-02-20 19:46:37 +0100

    compress fix

---
+++ Test Code/FileCompression/FileCompression/ByteCompressor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using SevenZip;
+using System.Text;
+
+namespace FileCompression
+{
+    public class ByteCompressor
+    {
+        public ByteCompressor()
+        {
+        }
+
+        public byte[] CompressBytes(byte[] inData)
+        {
+            Console.WriteLine("Original data is {0} bytes", inData.Length);
+            byte[] Compressed = SevenZip.Compression.LZMA.SevenZipHelper.Compress(inData);
+            Console.WriteLine("Compressed data is {0} bytes", Compressed.Length);
+
+            return Compressed;
+
+        }
+
+        public byte[] DecompressBytes(byte[] inData)
+        {
+            Console.WriteLine("Compressed data is {0} bytes", inData.Length);
+
+            byte[] Decompressed = SevenZip.Compression.LZMA.SevenZipHelper.Decompress(inData);
+            Console.WriteLine("Decompressed data is {0} bytes", Decompressed.Length);
+
+            return Decompressed;
+        }
+
+        public void CompressFile(string inPath, string outPath)
+        {
+            Stream inStream = File.Open(inPath, FileMode.Open);
+            Stream outStream = File.Open(outPath, FileMode.CreateNew);
+
+            byte[] buffer = new byte[1024 * 1024];
+            MemoryStream bufferStream = new MemoryStream();
+            inStream.CopyTo(bufferStream, 1024);
+
+            buffer = bufferStream.ToArray();
+
+
+
+        }
+
+
+    }
+
+}
--- Test Code/FileCompression/FileCompression/Program.cs
+++ Test Code/FileCompression/FileCompression/Program.cs
@@ -1,34 +1,14 @@
 ﻿using System;
-using System.IO.Compression;
-using SevenZip;
+using System.Text;
+
 namespace FileCompression
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            string mode = "";
 
-            Compressor coder = new Compressor();
-
-            while (mode != "E" && mode != "C")
-            {
-                Console.WriteLine("Compress or extract? (C/E)");
-                mode = Console.ReadLine();
-            }
-
-            if (mode == "C")
-            {
-                coder.Compress("", "");
-            }
-            else if (mode == "E")
-            {
-                coder.Extract("", "");
-            }
-            else
-            {
-                Console.WriteLine("Unknown error");
-            }
         }
     }
+
 }
