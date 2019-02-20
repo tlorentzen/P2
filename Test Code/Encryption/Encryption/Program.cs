c92d61586590088d1ff07e7535a1551c6a150f27
Alexander Nykjær <alax0013@gmail.com>
2019-02-20 11:41:16 +0100

    Now there is an if statement @Rosmarium

--- Test Code/Encryption/Encryption/Program.cs
+++ Test Code/Encryption/Encryption/Program.cs
@@ -5,15 +5,33 @@ namespace Encryption{
         public static void Main(string[] args){
             string Path = "./EncryptMe";
             string Extension = ".png";
-            FileEncryption File = new FileEncryption(Path,Extension);
+            int input;
+            FileEncryption File = new FileEncryption(Path, Extension);
             Console.WriteLine("Please enter your username");
             var password = Console.ReadLine();
-            Console.WriteLine("Encryption started");
-            File.doEncrypt(password);
-            Console.WriteLine("Encryption done");
-            Console.WriteLine("Decryption Started");
-            File.doDecrypt(password);
-            Console.WriteLine("Encryption done");
+            Console.WriteLine("Do you want to encrypt(1), or decrypt(2)?");
+            try{input = Convert.ToInt16(Console.ReadLine()); }
+            catch (Exception e){
+                Console.WriteLine("That was not a number");
+                Console.WriteLine(e);
+                throw;
+            }
+            switch (input){
+                case 1:
+                    Console.WriteLine("Encryption started");
+                    File.doEncrypt(password);
+                    Console.WriteLine("Encryption done");
+                    break;
+                case 2:
+
+                    Console.WriteLine("Decryption Started");
+                    File.doDecrypt(password);
+                    Console.WriteLine("Encryption done");
+                    break;
+                default:
+                    Console.WriteLine("Not an option");
+                    break;
+            }
         }
     }
 }
