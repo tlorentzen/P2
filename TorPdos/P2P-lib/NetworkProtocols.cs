16325681223e151582850adc830ac299df5bdd71
Niels Vistisen <42961494+Nielswps@users.noreply.github.com>
2019-04-01 15:58:13 +0200

    WIP TransferStatusMessage

--- TorPdos/P2P-lib/NetworkProtocols.cs
+++ TorPdos/P2P-lib/NetworkProtocols.cs
@@ -10,16 +10,40 @@ using System.Net.Sockets;
 using P2P_lib.Messages;
 using Index_lib;
 using P2P_lib;
-using TorPdos;
 
 namespace P2P_lib {
     public class NetworkProtocols {
-        public int UploadFileToNetwork (string filePath, int seed, Network network) {
+        Receiver receiver;
+        public void UploadFileToNetwork (string filePath, int seed, int copies, Network network) {
             List<Peer> peerlist = network.getPeerList();
-            for (int i = 0; i < seed; i++) {
-                
+            UploadMessage upload = new UploadMessage(peerlist[seed].GetIP());
+            upload.filesize = new FileInfo(filePath).Length;
+            upload.filename = new FileInfo(filePath).Name;
+            upload.filehash = DiskHelper.CreateMD5(filePath);
+            upload.path = filePath;
+            upload.type = Messages.TypeCode.REQUEST;
+            upload.statuscode = Messages.StatusCode.OK;
+            upload.port = NetworkHelper.getAvailablePort(55000, 56000);
+            receiver = new Receiver(upload.port);
+            receiver.start();
+            receiver.MessageReceived += Receiver_MessageReceived;
+            upload.Send();
+        }
+
+        private void Receiver_MessageReceived(BaseMessage msg) {
+            if (msg.GetMessageType() == typeof(UploadMessage)) {
+                UploadMessage upload = (UploadMessage)msg;
+
+                if (upload.type.Equals(Messages.TypeCode.REQUEST)) {
+                    if (DiskHelper.GetTotalFreeSpace("C:\\") > upload.filesize) {
+                        upload.statuscode = StatusCode.ACCEPTED;
+                    } else {
+                        upload.statuscode = StatusCode.INSUFFICIENT_STORAGE;
+                    }
+                    upload.CreateReply();
+                    upload.Send();
+                }
             }
-            return seed;
         }
     }
 }
