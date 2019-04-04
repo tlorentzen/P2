791ee4fa937b2f2760e8dee3d4d145a3e73fb3d4
Alexander Nykjær <alax0013@gmail.com>
2019-04-04 15:30:51 +0200

    Small tweaks

--- TorPdos/TorPdos/Properties/Resources.Designer.cs
+++ TorPdos/TorPdos/Properties/Resources.Designer.cs
@@ -24,6 +24,7 @@ namespace TorPdos.Properties {
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
         
+        
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
