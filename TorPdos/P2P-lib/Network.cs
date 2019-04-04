89a72e3fedfe70975533f1a68b2c62795a2ed8b2
Alexander Nykjær <alax0013@gmail.com>
2019-04-04 12:50:50 +0200

    Cleaned unnused call.

--- TorPdos/P2P-lib/Network.cs
+++ TorPdos/P2P-lib/Network.cs
@@ -254,9 +254,6 @@ namespace P2P_lib{
                     download.CreateReply();
                     download.statuscode = StatusCode.ACCEPTED;
                     
-                    NetworkPorts ports = new NetworkPorts();
-                    
-                    
                     download.Send();
                 } else{
                     download.CreateReply();
