44e8b779967b719f11460271b98da0f06a60ce69
AdrianPlesner <33195961+AdrianPlesner@users.noreply.github.com>
2019-04-09 13:22:59 +0200

    Hiddenfolder Unit tests

---
+++ TorPdos/TorPdos.TEST/IDHandelerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ID_lib;
+
+namespace TorPdos.TEST
+{
+    [TestClass]
+    public class IDHandelerTest
+    {
+        static string path = "TEST";
+        static string Password = "Password";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+    
+        }
+    }
+}
