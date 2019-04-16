e8cd63ee99b067687051ef0d8f51f694a7e0bf98
AdrianPlesner <33195961+AdrianPlesner@users.noreply.github.com>
2019-04-16 15:07:31 +0200

    Tests

--- TorPdos/TorPdos.TEST/IDHandelerTest.cs
+++ TorPdos/TorPdos.TEST/IDHandelerTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ID_lib;
+using System.IO;
 
 namespace TorPdos.TEST
 {
@@ -11,9 +12,58 @@ namespace TorPdos.TEST
         static string Password = "Password";
 
         [TestMethod]
-        public void TestMethod1()
+        public void GenereateUUIDIsRandom()
         {
-    
+            string notExpected = IdHandler.createUser(path, Password, null);
+            System.Threading.Thread.Sleep(5000);
+            string actual = IdHandler.createUser(path, Password, null);
+
+            IdHandler.removeUser(path);
+
+            Assert.AreNotEqual(notExpected, actual);
         }
+
+        [TestMethod]
+        public void UserDataFileCreated()
+        {
+            IdHandler.createUser(path, Password, null);
+
+            bool result = File.Exists(path + @"\userdata");
+            IdHandler.removeUser(path);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidateUser()
+        {
+            string uuid = IdHandler.createUser(path, Password, null);
+            bool result = IdHandler.isValidUser(path, uuid, Password);
+            IdHandler.removeUser(path);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void WrongPasswordInvaildUser()
+        {
+            string uuid = IdHandler.createUser(path, Password, null);
+            bool result = IdHandler.isValidUser(path, uuid, "wrong");
+            IdHandler.removeUser(path);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GetUUidIsCorrect()
+        {
+            string expected = IdHandler.createUser(path, Password, null);
+            string result = IdHandler.getUuid(path);
+            IdHandler.removeUser(path);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        
     }
 }
