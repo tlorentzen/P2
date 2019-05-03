95963c966bf7b71dec31eed3dac1e044bd62bd29
Thomas Lorentzen <thomas@tlorentzen.net>
2019-05-03 12:59:01 +0200

    Update NetworkPorts.cs

--- TorPdos/P2P-lib/NetworkPorts.cs
+++ TorPdos/P2P-lib/NetworkPorts.cs
@@ -8,7 +8,7 @@ namespace P2P_lib{
 
         public int GetAvailablePort(int beginRange = 50000, int endRange = 65535){
             for (int i = beginRange; i <= endRange; i++){
-                if (this.IsPortAvailable(i) && !ports.Contains(i)){
+                if (IsPortAvailable(i) && !ports.Contains(i)){
                     ports.Add(i);
                     return i;
                 }
@@ -23,7 +23,7 @@ namespace P2P_lib{
             }
         }
 
-        public bool IsPortAvailable(int port){
+        public static bool IsPortAvailable(int port){
             bool isAvailable = true;
             // https://stackoverflow.com/questions/570098/in-c-how-to-check-if-a-tcp-port-is-available
             // Evaluate current system tcp connections. This is the same information provided
