62ec3c3ac80de04874b50e79e60294c14ffad5ee
Niels Vistisen <42961494+Nielswps@users.noreply.github.com>
2019-05-06 12:51:14 +0200

    NetworkPorts also checks for TCP and UDP listeners

--- TorPdos/P2P-lib/NetworkPorts.cs
+++ TorPdos/P2P-lib/NetworkPorts.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using System.Net.NetworkInformation;
 
-namespace P2P_lib{
-    public class NetworkPorts{
+namespace P2P_lib {
+    public class NetworkPorts {
         private List<int> ports = new List<int>();
 
-        public int GetAvailablePort(int beginRange = 50000, int endRange = 65535){
-            for (int i = beginRange; i <= endRange; i++){
-                if (IsPortAvailable(i) && !ports.Contains(i)){
+        public int GetAvailablePort(int beginRange = 50000, int endRange = 65535) {
+            for (int i = beginRange; i <= endRange; i++) {
+                if (IsPortAvailable(i) && !ports.Contains(i)) {
                     ports.Add(i);
                     return i;
                 }
@@ -16,26 +17,47 @@ namespace P2P_lib{
             return 0;
         }
 
-        public void Release(int port){
-            if (this.ports.Contains(port)){
+        public void Release(int port) {
+            if (this.ports.Contains(port)) {
                 this.ports.Remove(port);
             }
         }
 
-        public static bool IsPortAvailable(int port){
+        public static bool IsPortAvailable(int port) {
             bool isAvailable = true;
+            // This is a hybrid of the two following links
             // https://stackoverflow.com/questions/570098/in-c-how-to-check-if-a-tcp-port-is-available
-            // Evaluate current system tcp connections. This is the same information provided
-            // by the netstat command line application, just in .Net strongly-typed object
-            // form.  We will look through the list, and if our port we would like to use
+            // https://gist.github.com/jrusbatch/4211535
+            // Evaluate current system tcp connections, TCP Listeners and UDP Listeners.
+            // We will look through the list, and if our port we would like to use
             // in our TcpClient is occupied, we will set isAvailable to false.
             IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
             TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+            IPEndPoint[] tcpListenerEndPoints = ipGlobalProperties.GetActiveTcpListeners();
+            IPEndPoint[] udpListenerEndPoints = ipGlobalProperties.GetActiveUdpListeners();
 
-            foreach (TcpConnectionInformation tcpi in tcpConnInfoArray){
-                if (tcpi.LocalEndPoint.Port == port){
+            foreach (TcpConnectionInformation tcpi in tcpConnInfoArray) {
+                if (tcpi.LocalEndPoint.Port == port) {
                     isAvailable = false;
                     break;
+                }
+            }
+
+            if (isAvailable) {
+                foreach (IPEndPoint tcpEndPoint in tcpListenerEndPoints) {
+                    if (tcpEndPoint.Port == port) {
+                        isAvailable = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isAvailable) {
+                foreach (IPEndPoint udpEndPoint in udpListenerEndPoints) {
+                    if (udpEndPoint.Port == port) {
+                        isAvailable = false;
+                        break;
+                    }
                 }
             }
 
