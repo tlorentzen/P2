2fa9e44735bb468c77533cda0aa02e657bd166ec
Niels Vistisen <42961494+Nielswps@users.noreply.github.com>
2019-05-20 14:10:54 +0200

    File spreading fixed WIP

--- TorPdos/P2P-lib/Handlers/FileHandlers/FileUploader.cs
+++ TorPdos/P2P-lib/Handlers/FileHandlers/FileUploader.cs
@@ -28,7 +28,7 @@ namespace P2P_lib
 
         public bool Push(P2PChunk chunk, string chunk_path, int numberOfRecevingPeers = 10, int receiverOffset = 0) {
             this._port = _ports.GetAvailablePort();
-            List<Peer> peers = this.GetPeers(numberOfRecevingPeers);
+            List<Peer> peers = this.GetPeers();
             FileInfo fileInfo = new FileInfo(chunk_path);
             Listener listener = new Listener(this._port);
             bool sendToAll = true;
@@ -63,7 +63,7 @@ namespace P2P_lib
             return sendToAll;
         }
 
-        private List<Peer> GetPeers(int count) {
+        private List<Peer> GetPeers(int count = 100) {
             List<Peer> topPeers = _peers.Values.Where(peer => peer.IsOnline() == true).ToList<Peer>();
             topPeers.Sort(new ComparePeersByRating());
             if (topPeers.Count > 0) {
